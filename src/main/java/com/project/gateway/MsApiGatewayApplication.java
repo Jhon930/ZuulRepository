b9cfd15527912f131aee80814244d2cd9b362b0f
Jhon930 <jhonnychamoli87@gmail.com>
2019-12-18 17:50:42 -0500

    changing api gateway from zuul to spring cloud gateway

--- src/main/java/com/project/gateway/MsApiGatewayApplication.java
+++ src/main/java/com/project/gateway/MsApiGatewayApplication.java
@@ -3,11 +3,9 @@ package com.project.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy 
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
