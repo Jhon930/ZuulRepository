93d73c2164f532d683bab8735c18249aae73c492
Jhon930 <jhonnychamoli87@gmail.com>
2020-01-02 18:58:29 -0500

    updating bootstrap properties file

--- src/main/java/com/project/gateway/MsApiGatewayApplication.java
+++ src/main/java/com/project/gateway/MsApiGatewayApplication.java
@@ -3,13 +3,15 @@ package com.project.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableZuulProxy
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsApiGatewayApplication.class, args);
 	}
+	
 
 }
